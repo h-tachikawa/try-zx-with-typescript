9695abde504ad114eb146e5e7d6faa3cf69fe91e
tachikawa <tachikawa@feedforce.jp>
2021-12-25 21:40:50 +0900

    Mod script

--- script.ts
+++ script.ts
@@ -1,6 +1,15 @@
 #!/usr/bin/node
-import { $, ProcessOutput } from "zx";
+
+import { $, ProcessOutput, chalk, question } from "zx";
+const YES = "y";
 
 $.verbose = false;
 const list: ProcessOutput = await $`ls -la`;
-console.log(list);
+console.log(chalk.cyanBright("This script has compiled by TypeScript."));
+const answer = await question("To run the script, please type 'y'", {
+  choices: ["y", "N"],
+});
+
+if (answer === YES) {
+  console.log(list.stdout);
+}
